6a9fd9b8930ac4957cc57b432353af667743e3bd
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-02 23:44:38 +0000

    babel added and working

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import x from "./test";
-
-console.log(`i importd  ${x} from test.js ----- webpack server working`);
+const y = 44;
+console.log(`i importd  ${x} from test.js ----- webpack server working  ${y}`);
