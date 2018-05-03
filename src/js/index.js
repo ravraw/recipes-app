e37d99447a192a19b54c1d9d3950cdb34327c718
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-03 21:49:21 +0000

    search results are rendering

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,41 @@
 import Search from "./models/Search";
-const search = new Search("pizza");
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 
-search.getResults();
-console.log(search);
+/**GLOBAL STATE
+ * -search object
+ * - current reciepe object
+ * -shopping list object
+ * -liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+  //1. Get query from view.
+  const query = searchView.getInput(); // todo
+  console.log(query);
+
+  if (query) {
+    //2 create new Search object and add to state
+    state.search = new Search(query);
+  }
+
+  //3  prepare UI for result
+  searchView.clearInput();
+  searchView.clearResults();
+
+  //4 search for recipes
+  await state.search.getResults();
+
+  //5  render results on UI
+  console.log(state.search.result);
+  searchView.renderResults(state.search.result);
+};
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
+
+const search = new Search("pizza");
